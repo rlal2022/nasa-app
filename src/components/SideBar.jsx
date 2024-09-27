fc76e099052082c0a46ab5111bdde8b620f78a0d
rlal2022 <lrahul2010@gmail.com>
2024-09-27 01:05:36 -0700

    sidebar css done

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,15 +2,17 @@ import React from "react";
 
 const SideBar = () => {
   return (
-    <div>
-      <h2>The Moon at night</h2>
-      <div>
-        <p>Description</p>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-          exercitationem explicabo reprehenderit provident quae repellat
-          perferendis tempora inventore odit ab?
-        </p>
+    <div className="sidebar">
+      <div className="bgOverlay"></div>
+      <div className="sidebarContents">
+        <div>
+          <p>Description</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+            consectetur molestias corporis dolor sed, amet soluta omnis veniam
+            eius harum.
+          </p>
+        </div>
       </div>
     </div>
   );
